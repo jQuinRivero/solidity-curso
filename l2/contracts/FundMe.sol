bf5145bf8b1d51534b535b2d17a9098ef7f51fd5
unknown <joaquinrivero94@gmail.com>
2022-05-14 12:11:15 -0300

    added withdraw and validations

--- l2/contracts/FundMe.sol
+++ l2/contracts/FundMe.sol
@@ -3,13 +3,24 @@
 pragma solidity >= 0.6.6 < 0.9.0;
 
 import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
+import "@chainlink/contracts/srv/v0.6/vendor/SafeMathChainLink.sol";
 
 contract FundMe {
-
+    using SafeMathChainlink for uint256;
+     
     mapping(address => uint256) public addressToAmountFunded;
+    address[] public funders;
+    address public owner;
+
+    constructor() public {
+        owner = msg.sender;
+    }
 
     function fund() public payable { // payable para que acepte payments
+        uint256 minUSD = 50 * 10 ** 18; // min 50 USD
+        require(getConversionRate(msg.value) >= minUSD, "Transacción no llega al mínimo de 50 USD"); // estilo assert
         addressToAmountFunded[msg.sender] += msg.value;
+        funders.push(msg.sender);
     }
 
     function getVersion() public view returns (uint256) {
@@ -28,5 +39,21 @@ contract FundMe {
         uint256 ethAmountInUsd = (ethPrice *ethAmount) / 1000000000000000000;
 
         return ethAmountInUsd;
+    }
+
+    modifier onlyOwner { // permite modificar otros métodos del contrato
+        require(msg.sender == owner);
+        _;
+    }
+
+    function withdraw() payable onlyOwner public { 
+        msg.sender.transfer(address(this).balance);
+        
+        for(uint256 funderIndex=0; funderIndex < funders.length; funderIndex++) {
+            address funder = funders[funderIndex];
+            addressToAmountFunded[funder] = 0;
+        } // reset de balance de senders
+
+        funders = new address[](0);
     }
 }
